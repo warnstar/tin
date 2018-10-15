4d683794dbbe8117f32dd28f86fd6afe5f29e5ac
wchuang <wchuang@aliyun.com>
2018-10-16 00:02:16 +0800

    add request log middleware

---
+++ app/common/middlewares/RequestLogMiddleware.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of Tin.
+ */
+namespace app\common\middlewares;
+
+use Tin\Http\Request;
+use Tin\Middleware\Middleware;
+
+class RequestLogMiddleware extends Middleware
+{
+    public function handle(Request $request)
+    {
+    }
+}
